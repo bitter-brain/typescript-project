4ab576f3f4f056ce2a844aae4b7f462c79c428ca
Boris Alekseev <borisalekseev95@gmail.com>
2026-03-11 15:36:26 +0200

    fix lint issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import App from "./app/App";
-import { store } from "./app/store";
-import "./styles/main.scss";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import App from './app/App'
+import { store } from './app/store'
+import './styles/main.scss'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -11,4 +11,4 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <App />
     </Provider>
   </React.StrictMode>
-);
+)
